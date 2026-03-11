7b98a811d952a8f08fcca4913ac0eaf18fd59af4
IKolmykov <zabyz1337@gmail.com>
2026-03-11 12:49:40 +0100

    add Header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Notes App</h1>
+    </header>
+  );
+};
+
+export default Header;
